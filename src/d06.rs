eddf85431429a5bbc24c5e8c64bf4358d3d1deec
Joonas Sarajärvi <muep@iki.fi>
2024-12-07 15:58:02 +0100

    day 06 part 2 WIP

--- src/d06.rs
+++ src/d06.rs
@@ -1,3 +1,5 @@
+use crate::d06::Place::Obstruction;
+use std::collections::HashSet;
 use std::io::{BufRead, BufReader, Read};
 use std::iter::successors;
 
@@ -8,18 +10,18 @@ enum Place {
     Traversable(u8),
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Pos {
     col: i16,
     row: i16,
 }
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 struct Speed {
     right: i16,
     down: i16,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 struct Guard {
     pos: Pos,
     spd: Speed,
@@ -127,9 +129,16 @@ fn forward(Pos { col, row }: Pos, Speed { down, right }: Speed) -> Pos {
     }
 }
 
+fn step_candidates(s: Speed) -> [Speed; 4] {
+    let s1 = turn(s);
+    let s2 = turn(s1);
+    let s3 = turn(s2);
+    [s, s1, s2, s3]
+}
+
 fn step(g: Guard, mut m: Map) -> (Guard, Map) {
-    let (spd, pos) = successors(Some(g.spd), |s| Some(turn(*s)))
-        .take(4)
+    let (spd, pos) = step_candidates(g.spd)
+        .into_iter()
         .map(|spd| (spd, forward(g.pos, spd)))
         .find(|(_, pos)| match m.get(*pos) {
             Place::Obstruction => false,
@@ -174,6 +183,40 @@ fn printout(guard: Guard, map: &Map) {
     println!();
 }
 
+fn printout_with_loopers(guard: Guard, map: &Map, loopers: HashSet<Pos>) {
+    let Speed { down, right } = guard.spd;
+    let height = map.places.len() / map.width;
+    for row in 0..height as i16 {
+        for col in 0..map.width as i16 {
+            let pos = Pos { col, row };
+
+            let c = if pos == guard.pos {
+                match (down, right) {
+                    (0, 1) => '>',
+                    (0, -1) => '<',
+                    (1, 0) => 'v',
+                    (-1, 0) => '^',
+                    _ => '@',
+                }
+            } else if loopers.contains(&pos) {
+                'Ø'
+            } else {
+                match map.get(pos) {
+                    Place::Obstruction => '#',
+                    Place::Traversable(0) => '.',
+                    Place::Traversable(1) => '-',
+                    Place::Traversable(2) => '+',
+                    Place::Traversable(_) => '*',
+                    _ => panic!("Should not print outside the map"),
+                }
+            };
+            print!("{c}")
+        }
+        println!();
+    }
+    println!();
+}
+
 fn stat(map: &Map) -> (usize, usize, usize) {
     map.places.iter().copied().fold(
         (0usize, 0usize, 0usize),
@@ -186,24 +229,69 @@ fn stat(map: &Map) -> (usize, usize, usize) {
     )
 }
 
-fn part1(input: &mut dyn Read) -> u32 {
+fn run_to_completion(state: (Guard, Map), history: HashSet<Guard>) -> (bool, Guard, Map) {
+    successors(Some(state), |(g, m)| Some(step(*g, m.clone())))
+        .scan((history, false), |(history, did_loop), (g, m)| {
+            if *did_loop {
+                return None;
+            }
+
+            *did_loop = history.contains(&g);
+            history.insert(g);
+
+            if m.get(g.pos) == Place::Outside {
+                None
+            } else {
+                Some((*did_loop, g, m))
+            }
+        })
+        .last()
+        .unwrap()
+}
+
+fn part1(input: &mut dyn Read) -> ((u32, u32, u32), Guard, Map) {
     let (guard, map) = successors(Some(load(input)), |(g, m)| Some(step(*g, m.clone())))
         .take_while(|(g, m)| m.get(g.pos) != Place::Outside)
         .last()
         .unwrap();
-
-    printout(guard, &map);
-    let (obsctuctions, unvisited, visited) = stat(&map);
-    println!("obstructed: {obsctuctions}\nunvisited: {unvisited}\nvisited: {visited}");
-    visited as u32
+    let (o, u, v) = stat(&map);
+    ((o as u32, u as u32, v as u32), guard, map)
 }
 
 fn part2(input: &mut dyn Read) -> u32 {
-    BufReader::new(input).lines().count() as u32
+    let (g0, m) = load(input);
+    let start_pos = g0.pos;
+    let (_,loopers) = successors(Some((g0, m.clone())), |(g, m)| Some(step(*g, m.clone())))
+        .take_while(|(g, m)| m.get(g.pos) != Place::Outside)
+        .fold(
+            (HashSet::new(), HashSet::new()),
+            |(mut past, mut loopers), (g, m)| {
+                let fwd_pos = forward(g.pos, g.spd);
+
+                if fwd_pos != start_pos {
+                    if let Place::Traversable(_) = m.get(fwd_pos) {
+                        let mut m2 = m.clone();
+                        m2.places[fwd_pos.row as usize * m2.width + fwd_pos.col as usize] =
+                            Obstruction;
+                        if let (true, _, _) = run_to_completion((g0, m2), HashSet::new()) {
+                            loopers.insert(fwd_pos);
+                        }
+                    }
+                }
+
+                past.insert(g);
+                (past, loopers)
+            },
+        );
+
+        loopers
+        .len() as u32
 }
 
 pub fn run_part1(input: &mut dyn Read) {
-    println!("{}", part1(input));
+    let ((obstructions, unvisited, visited), guard, map) = part1(input);
+    printout(guard, &map);
+    println!("obstructed: {obstructions}\nunvisited: {unvisited}\nvisited: {visited}");
 }
 
 pub fn run_part2(input: &mut dyn Read) {
@@ -234,28 +322,28 @@ mod tests {
     #[test]
     fn test_part1_example() {
         let mut f = File::open("input/d06-e.txt").unwrap();
-        let result = part1(&mut f);
+        let ((_, _, result), _, _) = part1(&mut f);
         assert_eq!(result, 41);
     }
 
     #[test]
     fn test_part1_full() {
         let mut f = File::open("input/d06-f.txt").unwrap();
-        let result = part1(&mut f);
+        let ((_, _, result), _, _) = part1(&mut f);
         assert_eq!(result, 5086);
     }
 
     #[test]
     fn test_part2_example() {
-        let mut f = File::open("input/d00-e.txt").unwrap();
+        let mut f = File::open("input/d06-e.txt").unwrap();
         let result = part2(&mut f);
-        assert_eq!(result, 0);
+        assert_eq!(result, 6);
     }
 
     #[test]
     fn test_part2_full() {
-        let mut f = File::open("input/d00-f.txt").unwrap();
+        let mut f = File::open("input/d06-f.txt").unwrap();
         let result = part2(&mut f);
-        assert_eq!(result, 0);
+        assert_eq!(result, 1831);
     }
 }
