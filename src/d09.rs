c4b8986b9c51354d9ad253bd50f19ffea717a683
Joonas Sarajärvi <muep@iki.fi>
2024-12-11 21:15:46 +0100

    day 09 part 1 (slow version)

--- src/d09.rs
+++ src/d09.rs
@@ -1,7 +1,80 @@
 use std::io::{BufRead, BufReader, Read};
+use std::iter::{repeat, successors};
 
-fn part1(input: &mut dyn Read) -> u32 {
-    BufReader::new(input).lines().count() as u32
+enum LoadNext {
+    ContentForFile(u16),
+    SpaceAfterFile(u16),
+}
+
+fn load(input: &mut dyn Read) -> Vec<Option<u16>> {
+    input
+        .bytes()
+        .filter_map(|b| match b {
+            Ok(n) => {
+                if n.is_ascii_digit() {
+                    Some((n - b'0') as u16)
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        })
+        .fold(
+            (LoadNext::ContentForFile(0), Vec::new()),
+            |(next, mut buf), num_items| match next {
+                LoadNext::ContentForFile(id) => {
+                    buf.extend(repeat(Some(id)).take(num_items as usize));
+                    (LoadNext::SpaceAfterFile(id), buf)
+                }
+                LoadNext::SpaceAfterFile(id) => {
+                    buf.extend(repeat(None).take(num_items as usize));
+                    (LoadNext::ContentForFile(id + 1), buf)
+                }
+            },
+        )
+        .1
+}
+
+fn fragment(mut disk: Vec<Option<u16>>) -> (bool, Vec<Option<u16>>) {
+    let next_free = disk
+        .iter()
+        .copied()
+        .enumerate()
+        .find(|(_, slot)| slot.is_none())
+        .map(|(pos, _)| pos)
+        .unwrap_or(0);
+
+    let next_to_move = disk
+        .iter()
+        .copied()
+        .enumerate()
+        .rfind(|(_, slot)| slot.is_some())
+        .map(|(pos, _)| pos)
+        .unwrap_or(0);
+
+    if next_free >= next_to_move {
+        return (false, disk);
+    }
+
+    disk.swap(next_to_move, next_free);
+    (true, disk)
+}
+
+fn part1(input: &mut dyn Read) -> u64 {
+    successors(Some(load(input)), |disk| {
+        let (did_change, disk) = fragment(disk.clone());
+        if did_change {
+            Some(disk)
+        } else {
+            None
+        }
+    })
+    .last()
+    .unwrap()
+    .into_iter()
+    .enumerate()
+    .map(|(pos, id)| id.map(|n| n as u64 * pos as u64).unwrap_or(0))
+    .sum()
 }
 
 fn part2(input: &mut dyn Read) -> u32 {
@@ -22,17 +95,48 @@ mod tests {
     use std::fs::File;
 
     #[test]
+    fn test_parse_digit() {
+        let digit = b'4';
+        let num = digit - b'0';
+
+        assert_eq!(num, 4);
+    }
+
+    #[test]
+    fn test_load_example() {
+        let mut f = File::open("input/d09-e.txt").unwrap();
+        let disk = load(&mut f);
+        assert_eq!(
+            disk,
+            "00...111...2...333.44.5555.6666.777.888899"
+                .bytes()
+                .map(|c| match c {
+                    b'.' => None,
+                    n => Some(n as u16 - b'0' as u16),
+                })
+                .collect::<Vec<Option<u16>>>()
+        );
+    }
+
+    #[test]
+    fn test_load_full() {
+        let mut f = File::open("input/d09-f.txt").unwrap();
+        let disk = load(&mut f);
+        assert_eq!(disk.len(), 95177);
+    }
+
+    #[test]
     fn test_part1_example() {
-        let mut f = File::open("input/d00-e.txt").unwrap();
+        let mut f = File::open("input/d09-e.txt").unwrap();
         let result = part1(&mut f);
-        assert_eq!(result, 0);
+        assert_eq!(result, 1928);
     }
 
     #[test]
     fn test_part1_full() {
-        let mut f = File::open("input/d00-f.txt").unwrap();
+        let mut f = File::open("input/d09-f.txt").unwrap();
         let result = part1(&mut f);
-        assert_eq!(result, 0);
+        assert_eq!(result, 6385338159127);
     }
 
     #[test]
